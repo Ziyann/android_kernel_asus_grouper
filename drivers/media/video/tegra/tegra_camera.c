244a44108f28c5f1312189ab72d631a4f649df3a
Jihoon Bang <jbang@nvidia.com>
2012-06-19 16:12:00 -0700

    WAR: gr3d: limit 3d clock when camera is on
    As WAR, limit 3d clock frequency and emc clock frequency
    when camera is on and chip is AP37. 3d clock is set to
    361MHz and 437MHz is requested for emc clock with this
    change. This change allows 3d to request 1.1V in Core
    instead of 1.3V in AP37.
    
    Bug 1001262
    Bug 1019309
    
    Signed-off-by: Jihoon Bang <jbang@nvidia.com>
    Change-Id: I1dbab3469a31ef603f7665fc5a6cb5c1999f89b6
    Reviewed-on: http://git-master/r/120480
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- drivers/media/video/tegra/tegra_camera.c
+++ drivers/media/video/tegra/tegra_camera.c
@@ -2,7 +2,7 @@
  * drivers/media/video/tegra/tegra_camera.c
  *
  * Copyright (C) 2010 Google, Inc.
- * Copyright (C) 2012 Nvidia Corp
+ * Copyright (c) 2010-2012, NVIDIA Corporation.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -60,6 +60,25 @@ struct tegra_camera_block {
 	int (*disable) (struct tegra_camera_dev *dev);
 	bool is_enabled;
 };
+
+/*
+ * Declare and define two static variables to provide hint to
+ * gr3d module
+ */
+static int tegra_camera_on;
+static struct tegra_camera_platform_data *pdata;
+
+int is_tegra_camera_on(void)
+{
+	if (pdata) {
+		if (pdata->limit_3d_emc_clk)
+			return tegra_camera_on;
+		else
+			return 0;
+	} else {
+		return 0;
+	}
+}
 
 static int tegra_camera_enable_clk(struct tegra_camera_dev *dev)
 {
@@ -232,6 +251,7 @@ static int tegra_camera_power_on(struct tegra_camera_dev *dev)
 			__func__);
 #endif
 	dev->power_on = 1;
+	tegra_camera_on = dev->power_on;
 	return ret;
 }
 
@@ -260,6 +280,7 @@ static int tegra_camera_power_off(struct tegra_camera_dev *dev)
 		}
 	}
 	dev->power_on = 0;
+	tegra_camera_on = dev->power_on;
 	return ret;
 }
 
@@ -450,6 +471,7 @@ static int tegra_camera_probe(struct platform_device *pdev)
 	mutex_unlock(&dev->tegra_camera_lock);
 
 	dev->dev = &pdev->dev;
+	pdata = pdev->dev.platform_data;
 
 	/* Get regulator pointer */
 #ifdef CONFIG_ARCH_TEGRA_2x_SOC
