510e933eceda96651d5e79d1a56d964431c2f60c
Bryan Wu <pengw@nvidia.com>
2013-01-16 17:29:03 -0800

    media: soc_camera: tegra: fix one building warning
    Change-Id: Ie685ad11a0847be5b51c752ec593aec857e2f0e2
    Signed-off-by: Bryan Wu <pengw@nvidia.com>
    Reviewed-on: http://git-master/r/200224
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Matthew Pedro <mapedro@nvidia.com>

--- drivers/media/video/tegra_v4l2_camera.c
+++ drivers/media/video/tegra_v4l2_camera.c
@@ -1085,7 +1085,7 @@ static int tegra_camera_videobuf_setup(struct vb2_queue *vq,
 	if (!*num_buffers)
 		*num_buffers = 2;
 
-	dev_dbg(icd->parent, "num_buffers=%u, size=%u\n",
+	dev_dbg(icd->parent, "num_buffers=%u, size=%lu\n",
 		*num_buffers, sizes[0]);
 
 	tegra_camera_capture_setup(pcdev);
