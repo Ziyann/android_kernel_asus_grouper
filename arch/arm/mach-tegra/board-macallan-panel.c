214b75a116eb2f4cd952dcc69c7745c4b535d571
Min-wuk Lee <mlee@nvidia.com>
2013-05-15 17:03:32 +0900

    ARM: tegra: remove duplicated fb2 clear on macallan, tegratab
    fb2 clear is duplicated since it is handled
    in tegra_init_hdmi
    
    Bug 1270114
    Bug 1301312
    
    Change-Id: Icf6033c4dc64edb2013fa95ecf711e35519b2e5a
    Signed-off-by: Min-wuk Lee <mlee@nvidia.com>
    Reviewed-on: http://git-master/r/228744
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/board-macallan-panel.c
+++ arch/arm/mach-tegra/board-macallan-panel.c
@@ -500,16 +500,6 @@ int __init macallan_panel_init(void)
 	__tegra_move_framebuffer(&macallan_nvmap_device,
 		tegra_fb_start, tegra_bootloader_fb_start,
 			min(tegra_fb_size, tegra_bootloader_fb_size));
-	/*
-	 * If the bootloader fb2 is valid, copy it to the fb2, or else
-	 * clear fb2 to avoid garbage on dispaly2.
-	 */
-	if (tegra_bootloader_fb2_size)
-		tegra_move_framebuffer(tegra_fb2_start,
-			tegra_bootloader_fb2_start,
-			min(tegra_fb2_size, tegra_bootloader_fb2_size));
-	else
-		tegra_clear_framebuffer(tegra_fb2_start, tegra_fb2_size);
 
 	macallan_disp1_device.dev.parent = &phost1x->dev;
 	err = platform_device_register(&macallan_disp1_device);
