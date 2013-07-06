a0d6b6b01e1fb2fe9a34861da0b9eabdb2623397
Philip Rakity <prakity@nvidia.com>
2013-07-06 19:08:10 +0100

    sound: soc: rt56xx:  Fix compiler warning
    	missing #include <linux/module.h>
    
    Change-Id: I760ce2c55f3c1bc22821e583f1c8fce2d2ab74f4
    Signed-off-by: Philip Rakity <prakity@nvidia.com>
    Reviewed-on: http://git-master/r/245846
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- sound/soc/codecs/rt56xx_ioctl.c
+++ sound/soc/codecs/rt56xx_ioctl.c
@@ -4,11 +4,14 @@
  * Copyright (c) 2011-2013 REALTEK SEMICONDUCTOR CORP. All rights reserved.
  * Author: Bard Liao <bardliao@realtek.com>
  *
+ * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
 
+#include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <sound/soc.h>
 #include "rt56xx_ioctl.h"
