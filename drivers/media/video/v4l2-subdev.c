2cb6e92a27188355cb2747c2536b5b4c57dd0771
Philip Rakity <prakity@nvidia.com>
2013-07-08 14:23:25 +0100

    media: video: v4l Remove compile warning
    Change-Id: Ie1059c00226e2f948151c82d1db3a79527c27aaf
    Signed-off-by: Philip Rakity <prakity@nvidia.com>
    Reviewed-on: http://git-master/r/246133
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/media/video/v4l2-subdev.c
+++ drivers/media/video/v4l2-subdev.c
@@ -20,6 +20,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <linux/file.h>
 #include <linux/ioctl.h>
 #include <linux/slab.h>
 #include <linux/types.h>
