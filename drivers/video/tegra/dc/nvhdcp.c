9db90524db50dffce1f7f6e53a6b083270307556
David Yu <davyu@nvidia.com>
2013-08-29 14:51:01 +0900

    nvhdcp: avoid i2c transfer retry on suspend
    Bug 1346938
    
    Change-Id: I95d8806a6cd4fbb6060f0b6e5bcebc27f1c846fc
    Signed-off-by: David Yu <davyu@nvidia.com>
    Reviewed-on: http://git-master/r/267718
    (cherry picked from commit edf22c31968ec0658e1315f9c6d468f3961c9e1e)
    Reviewed-on: http://git-master/r/282193
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Gabby Lee <galee@nvidia.com>

--- drivers/video/tegra/dc/nvhdcp.c
+++ drivers/video/tegra/dc/nvhdcp.c
@@ -1073,9 +1073,9 @@ static int tegra_nvhdcp_on(struct tegra_nvhdcp *nvhdcp)
 
 static int tegra_nvhdcp_off(struct tegra_nvhdcp *nvhdcp)
 {
+	nvhdcp_set_plugged(nvhdcp, false);
 	mutex_lock(&nvhdcp->lock);
 	nvhdcp->state = STATE_OFF;
-	nvhdcp_set_plugged(nvhdcp, false);
 	mutex_unlock(&nvhdcp->lock);
 	wake_up_interruptible(&wq_worker);
 	flush_workqueue(nvhdcp->downstream_wq);
