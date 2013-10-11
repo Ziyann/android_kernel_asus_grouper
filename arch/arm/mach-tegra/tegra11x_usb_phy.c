5716e08b0a7bda84dcc6b06a1770978f2d3cbba4
Neil Patel <neilp@nvidia.com>
2013-10-11 14:31:43 -0400

    ARM: tegra: usb: add remote wakeup callback
    Drivers can take actions based on remote wakeup events to prevent a
    device or the bus from suspending before the incoming data is handled.
    
    Bug 1362837
    Bug 1430974
    
    Change-Id: I55cf26663cbf00bd2eccc60f18aa95ab5777e604
    Signed-off-by: Neil Patel <neilp@nvidia.com>
    Reviewed-on: http://git-master/r/289968
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Steve Lin <stlin@nvidia.com>
    Reviewed-by: Gray Lei <glei@nvidia.com>
    Reviewed-on: http://git-master/r/349731
    Reviewed-by: Martin Chi <mchi@nvidia.com>
    Tested-by: Martin Chi <mchi@nvidia.com>
    Reviewed-on: http://git-master/r/351588
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/tegra11x_usb_phy.c
+++ arch/arm/mach-tegra/tegra11x_usb_phy.c
@@ -1586,8 +1586,11 @@ static void uhsic_phy_restore_end(struct tegra_usb_phy *phy)
 	int wait_time_us = FPR_WAIT_TIME_US; /* FPR should be set by this time */
 	bool irq_disabled = false;
 	struct tegra_usb_pmc_data *pmc = &pmc_data[phy->inst];
+	bool remote_wakeup_detected;
 
 	DBG("%s(%d)\n", __func__, __LINE__);
+
+	remote_wakeup_detected = phy->pmc_remote_wakeup;
 
 	/*
 	 * check whether we wake up from the remote wake detected before putting
@@ -1666,6 +1669,9 @@ static void uhsic_phy_restore_end(struct tegra_usb_phy *phy)
 		pr_err("%s: timeout waiting for USB_USBCMD_RS\n", __func__);
 		return;
 	}
+	if (remote_wakeup_detected && phy->pdata->ops &&
+					phy->pdata->ops->post_remote_wakeup)
+		phy->pdata->ops->post_remote_wakeup();
 }
 
 static int uhsic_rail_enable(struct tegra_usb_phy *phy)
