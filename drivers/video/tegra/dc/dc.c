a3f76166b3aec341cb59a642ad7532905879335f
Min-wuk Lee <mlee@nvidia.com>
2013-10-18 16:39:19 +0900

    video: tegra: dc: nvhdcp off in shutdown
    If dc out type is hdmi, nvhdcp off is necessary ahead of
    dc disabling to avoid flicker in hdmi display.
    Having suspend which includes nvhdcp off, here as a
    workaround.
    
    Bug 1389592
    
    Change-Id: I1b9501890d75cb2681ed18c353b404fabb333d0c
    Signed-off-by: Min-wuk Lee <mlee@nvidia.com>
    Reviewed-on: http://git-master/r/301102
    (cherry picked from commit 23af567e569a6f0259b9ad848671201555042c07)
    Reviewed-on: http://git-master/r/302768
    Reviewed-by: Harry Hong <hhong@nvidia.com>
    Tested-by: Harry Hong <hhong@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -2690,6 +2690,14 @@ static void tegra_dc_shutdown(struct platform_device *ndev)
 	if (!dc || !dc->enabled)
 		return;
 
+	/*
+	 * To have nvhdcp off
+	 * FIXME: destroy is more desirable
+	 */
+	if (dc->out->type == TEGRA_DC_OUT_HDMI &&
+		dc->out_ops && dc->out_ops->suspend)
+		dc->out_ops->suspend(dc);
+
 	tegra_dc_disable(dc);
 }
 
