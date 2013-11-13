5771ccf19ad21cb9d38667cec4cb4953ea4b6604
David Jung <djung@nvidia.com>
2013-11-13 13:47:38 -0800

    Revert "Revert "Revert "input: touch: maxim_sti: boot fusion from init"""
    This reverts commit ea70be4d831ccb26d02eaf7ed79c085923c52e98.
    
    Bug 1404490
    
    Change-Id: I270124945ec1037fef8c54938dfcb285939b7471
    Signed-off-by: David Jung <djung@nvidia.com>
    Reviewed-on: http://git-master/r/330674
    Reviewed-by: Harry Hong <hhong@nvidia.com>
    Tested-by: Harry Hong <hhong@nvidia.com>

--- drivers/input/touchscreen/maxim_sti.c
+++ drivers/input/touchscreen/maxim_sti.c
@@ -70,6 +70,7 @@ struct dev_data {
 	u32                          nl_seq;
 	u8                           nl_mc_group_count;
 	bool                         nl_enabled;
+	bool                         start_fusion;
 	bool                         suspend_in_progress;
 	bool                         resume_in_progress;
 	bool                         expect_resume_ack;
@@ -1579,6 +1580,9 @@ static int processing_thread(void *arg)
 	struct dev_data         *dd = arg;
 	struct maxim_sti_pdata  *pdata = dd->spi->dev.platform_data;
 	struct sk_buff          *skb;
+	char                    *argv[] = { pdata->touch_fusion, "daemon",
+					    pdata->nl_family,
+					    pdata->config_file, NULL };
 	int                     ret, ret2;
 	bool                    fusion_dead;
 
@@ -1600,10 +1604,20 @@ static int processing_thread(void *arg)
 					PIDTYPE_PID) == NULL &&
 					!dd->suspend_in_progress) {
 			stop_scan_canned(dd);
+			dd->start_fusion = true;
 			dd->fusion_process = (pid_t)0;
 #if INPUT_ENABLE_DISABLE
 			dd->input_no_deconfig = true;
 #endif
+		}
+		if (dd->start_fusion) {
+			do {
+				ret = call_usermodehelper(argv[0], argv, NULL,
+							  UMH_WAIT_EXEC);
+				if (ret != 0)
+					msleep(100);
+			} while (ret != 0 && !kthread_should_stop());
+			dd->start_fusion = false;
 		}
 		if (kthread_should_stop())
 			break;
@@ -1837,6 +1851,7 @@ static int probe(struct spi_device *spi)
 	__pm_stay_awake(&dd->ws);
 #endif
 	/* start up Touch Fusion */
+	dd->start_fusion = true;
 	wake_up_process(dd->thread);
 	INFO("driver loaded; version %s; release date %s", DRIVER_VERSION,
 	     DRIVER_RELEASE);
