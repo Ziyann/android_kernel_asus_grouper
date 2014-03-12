1f134d81a0791757f5632b5745bd3945dcdb2f6d
Martin Chi <mchi@nvidia.com>
2014-03-12 12:44:37 +0800

    usb: hsic: add delay in phy resume for auto-resume
    add 10ms delay in hsic phy resume for auto-resume
    
    bug 1476774
    
    Change-Id: I54d0b46347084b3e41520f4c0f878a1336b54931
    Signed-off-by: Martin Chi <mchi@nvidia.com>
    Reviewed-on: http://git-master/r/380736
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-by: Gray Lei <glei@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/tegra11x_usb_phy.c
+++ arch/arm/mach-tegra/tegra11x_usb_phy.c
@@ -1643,7 +1643,8 @@ static void uhsic_phy_restore_end(struct tegra_usb_phy *phy)
 		local_irq_restore(flags);
 		mdelay(25);
 		local_irq_save(flags);
-	}
+	} else
+		mdelay(10);
 
 	pmc->pmc_ops->disable_pmc_bus_ctrl(pmc, 1);
 	phy->pmc_remote_wakeup = false;
