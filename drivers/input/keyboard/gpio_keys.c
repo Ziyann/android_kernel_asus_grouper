f02f9144023bec45df7819a3414670c48bcc8afc
Danny Song <dsong@nvidia.com>
2014-06-13 10:07:36 +0800

    driver:tn7c:add log to indicate the power key event
    Add log to indicate the power key event.
    Bug 200011910
    Bug 200012680
    
    Change-Id: I462d4f1655529c0e964b77812d55d248be541eec
    Signed-off-by: Danny Song <dsong@nvidia.com>
    Reviewed-on: http://git-master/r/422752
    GVS: Gerrit_Virtual_Submit
    Reviewed-on: http://git-master/r/435524

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -353,6 +353,8 @@ static void gpio_keys_gpio_report_event(struct gpio_button_data *bdata)
 		input_event(input, type, button->code, !!state);
 	}
 	input_sync(input);
+	printk("gpio key event is reported gpio=%d\n",button->code);
+
 }
 
 static void gpio_keys_gpio_work_func(struct work_struct *work)
