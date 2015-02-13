bd532dc461d9b4cf80a42fec7527748c46012d46
Ziyan <jaraidaniel@gmail.com>
2015-02-13 18:16:38 +0100

    arm: tegra: grouper: make embedded_sdio platform data conditional

--- arch/arm/mach-tegra/board-grouper-sdhci.c
+++ arch/arm/mach-tegra/board-grouper-sdhci.c
@@ -126,6 +126,7 @@ static struct resource sdhci_resource3[] = {
 	},
 };
 
+#ifdef CONFIG_MMC_EMBEDDED_SDIO
 static struct embedded_sdio_data embedded_sdio_data2 = {
 	.cccr   = {
 		.sdio_vsn       = 2,
@@ -140,13 +141,19 @@ static struct embedded_sdio_data embedded_sdio_data2 = {
 		.device         = 0x4330,
 	},
 };
+#endif
 
 static struct tegra_sdhci_platform_data tegra_sdhci_platform_data2 = {
 	.mmc_data = {
 		.register_status_notify	= grouper_wifi_status_register,
+#ifdef CONFIG_MMC_EMBEDDED_SDIO
 		.embedded_sdio = &embedded_sdio_data2,
+#endif
 		.built_in = 1,
 	},
+#ifndef CONFIG_MMC_EMBEDDED_SDIO
+	.pm_flags = MMC_PM_KEEP_POWER,
+#endif
 	.cd_gpio = -1,
 	.wp_gpio = -1,
 	.power_gpio = -1,
